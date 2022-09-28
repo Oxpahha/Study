36caab6b66ec1463eac93c39bd9cb7722141a6cc
Oxpa <saweec9@gmail.com>
2022-09-28 12:54:45 +0300

    4sem homework 3

--- GB/3.Module C#/4th seminar/homework_29/Program.cs
+++ GB/3.Module C#/4th seminar/homework_29/Program.cs
@@ -5,3 +5,34 @@
 // - Должна быть отдельная функция, которая создаёт массив и отдельная функция вывода
 // - Внутри функций (кроме функции вывода) НЕЛЬЗЯ использовать класс Console. 
 //   Только в основном блоке кода или в функции вывода
+
+// Console.WriteLine("Введите размер массива: ");
+// int arrayLength = int.Parse(Console.ReadLine() ?? "0");
+int arrayLength = 8;
+int[] array = new int[arrayLength];
+
+FillArray(array);
+PrintArray(array);
+
+void FillArray(int[] array)
+{
+    Random rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next();
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write(array[i]);
+        if (i == count - 1)
+            Console.WriteLine("]");
+        else
+            Console.Write(", ");
+    }
+}
