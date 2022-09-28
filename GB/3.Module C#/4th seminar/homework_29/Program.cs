f6e68832d5bf6cae4e8d79e344b86846964d4875
Oxpa <saweec9@gmail.com>
2022-09-28 13:41:25 +0300

    4sem homework 3 sorting

--- GB/3.Module C#/4th seminar/homework_29/Program.cs
+++ GB/3.Module C#/4th seminar/homework_29/Program.cs
@@ -13,13 +13,34 @@ int[] array = new int[arrayLength];
 
 FillArray(array);
 PrintArray(array);
+Sorting(array);
+PrintArray(array);
+
+void Sorting(int[] array)
+{
+    int arLength = array.Length;
+    int temp = 0;
+    for (int j = 0; j < arLength; j++)
+    {
+        for (int i = 0; i < arLength - 1; i++)
+        {
+            if (Math.Abs(array[i]) > Math.Abs(array[i + 1]))
+            {
+                temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+            }
+        }
+    }
+}
+
 
 void FillArray(int[] array)
 {
     Random rand = new Random();
     for (int i = 0; i < array.Length; i++)
     {
-        array[i] = rand.Next();
+        array[i] = rand.Next(-10,10);
     }
 }
 
