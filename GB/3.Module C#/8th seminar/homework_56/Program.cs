c940dd13c5873e8c509b77b71fdf210225382325
Oxpa <saweec9@gmail.com>
2022-10-12 18:04:23 +0300

    56 homework

--- GB/3.Module C#/8th seminar/homework_56/Program.cs
+++ GB/3.Module C#/8th seminar/homework_56/Program.cs
@@ -9,3 +9,61 @@
 
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с
 //  наименьшей суммой элементов: 1 строка
+
+Console.Write("Ведите кол-во строк: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Ведите кол-во колонн: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+int[,] array = new int[m, n];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine($"{RowWithLowestSumm(array)} строка");
+
+int RowWithLowestSumm(int[,] matrixArray)
+{
+    int min = 0;
+    int sum = int.MaxValue;
+    for (int i = 0; i < matrixArray.GetLength(0); i++)
+    {
+        int rowSumm = 0;
+        for (int j = 0; j < matrixArray.GetLength(1); j++)
+        {
+            rowSumm += matrixArray[i, j];
+        }
+        if (rowSumm < sum)
+        {
+            sum = rowSumm;
+            min = i+1;
+        }
+    }
+    return min;
+}
+
+void FillArray(int[,] matrixArray)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < matrixArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixArray.GetLength(1); j++)
+        {
+            matrixArray[i, j] = rnd.Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] matrixArray)
+{
+    for (int i = 0; i < matrixArray.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < matrixArray.GetLength(1); j++)
+        {
+            Console.Write(matrixArray[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine();
+    }
+}
