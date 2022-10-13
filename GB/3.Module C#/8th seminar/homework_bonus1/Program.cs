330ba6554c8f0d01aa1e885ba925147b40267c7c
Oxpa <saweec9@gmail.com>
2022-10-13 18:47:51 +0300

    8th bonus homework

--- GB/3.Module C#/8th seminar/homework_bonus1/Program.cs
+++ GB/3.Module C#/8th seminar/homework_bonus1/Program.cs
@@ -10,3 +10,85 @@
 // 9 4 2
 // 2 2 6
 // 3 4 7
+
+Console.Write("Ведите кол-во строк: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Ведите кол-во колонн: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+int[,] array = new int[m, n];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+PrintArray(CutColRowWithLowestElem(array));
+
+
+int[,] CutColRowWithLowestElem(int[,] matrixArray)
+{
+    int min = int.MaxValue;
+    int minJ = int.MaxValue;
+    int minI = int.MaxValue;
+
+    int[,] newArray = new int[matrixArray.GetLength(0) - 1, matrixArray.GetLength(1) - 1];
+    int[] array = new int[matrixArray.GetLength(0) - 1];
+
+    for (int i = 0; i < matrixArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixArray.GetLength(1); j++)
+        {
+            if (matrixArray[i, j] < min)
+            {
+                min = matrixArray[i, j];
+                minJ = j;
+                minI = i;
+            }
+        }
+    }
+
+    int l = 0;
+    for (int i = 0; i < matrixArray.GetLength(0); i++)
+    {
+        int k = 0;
+        for (int j = 0; j < matrixArray.GetLength(1); j++)
+        {
+            if (j != minJ)
+            { 
+                array[k] = matrixArray[i, j];
+                if(l < newArray.GetLength(0))
+                newArray[l, k] = array[k];
+                k++;
+            }
+        }
+        l++;
+        if (i == minI)
+            l--;
+    }
+    return newArray;
+}
+
+void FillArray(int[,] matrixArray)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < matrixArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixArray.GetLength(1); j++)
+        {
+            matrixArray[i, j] = rnd.Next(1, 100);
+        }
+    }
+}
+
+void PrintArray(int[,] matrixArray)
+{
+    for (int i = 0; i < matrixArray.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < matrixArray.GetLength(1); j++)
+        {
+            Console.Write(matrixArray[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine();
+    }
+}
