ad1ca446f81e5ffe5b7d166364ab5008d6bc4bdd
Oxpa <saweec9@gmail.com>
2022-10-15 01:33:21 +0300

    9sem 3

--- GB/3.Module C#/9th seminar/sem_Project3/Program.cs
+++ GB/3.Module C#/9th seminar/sem_Project3/Program.cs
@@ -1,70 +1,35 @@
-﻿// Задача 57: Составить частотный словарь элементов двумерного массива.
-// Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+﻿// Задан массив, в котором хранится двоичное представление числа.
+// Нужно вывести его десятичное представление {0,1,1,1,1} -> 15
 
 
+int[] binNum = { 0, 1, 1, 1, 1 };
+PrintArray(binNum);
 
-Console.Write("Ведите кол-во строк: ");
-int m = int.Parse(Console.ReadLine() ?? "0");
-Console.Write("Ведите кол-во колонн: ");
-int n = int.Parse(Console.ReadLine() ?? "0");
+Array.Reverse(binNum);
+PrintArray(binNum);
+BinToInt(binNum);
 
-int[,] array = new int[m, n];
-
-FillArray(array);
-PrintArray(array);
-Console.WriteLine();
-ElementCounter(array);
-
-
-void ElementCounter(int[,] matrixArray)
+void BinToInt(int[] BinArray)
 {
-    for (int k = 0; k < matrixArray.GetLength(0); k++)
+    double result = 0;
+
+    for (int i = 0; i < BinArray.Length; i++)
     {
-        for (int l = 0; l < matrixArray.GetLength(1); l++)
-        {
-            int temp = matrixArray[k, l];
-            int count = 0;
-            if (temp != 0)
-            {
-                for (int i = 0; i < matrixArray.GetLength(0); i++)
-                {
-                    for (int j = 0; j < matrixArray.GetLength(1); j++)
-                    {
-                        if (temp == matrixArray[i, j])
-                        {
-                            count++;
-                            matrixArray[i, j] = 0;
-                        }
-                    }
-                }
-                Console.WriteLine($"В массиве {temp} встречается {count} раз");
-            }
-        }
+        result += Math.Pow(2, i) * BinArray[i];
     }
+    Console.WriteLine(result);
 }
 
-void FillArray(int[,] matrixArray)
+void PrintArray(int[] array)
 {
-    Random rnd = new Random();
-    for (int i = 0; i < matrixArray.GetLength(0); i++)
+    int count = array.Length;
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
     {
-        for (int j = 0; j < matrixArray.GetLength(1); j++)
-        {
-            matrixArray[i, j] = rnd.Next(1, 10);
-        }
-    }
-}
-
-void PrintArray(int[,] matrixArray)
-{
-    for (int i = 0; i < matrixArray.GetLength(0); i++)
-    {
-        Console.Write("[ ");
-        for (int j = 0; j < matrixArray.GetLength(1); j++)
-        {
-            Console.Write(matrixArray[i, j] + " ");
-        }
-        Console.Write("]");
-        Console.WriteLine();
+        Console.Write(array[i]);
+        if (i == count - 1)
+            Console.WriteLine("]");
+        else
+            Console.Write(", ");
     }
 }
