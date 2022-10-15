702094f8b2f65928862135e274a786d00d2a2027
Oxpa <saweec9@gmail.com>
2022-10-16 00:30:55 +0300

    9th sem bonus

--- GB/3.Module C#/9th seminar/sem_Project4/Program.cs
+++ GB/3.Module C#/9th seminar/sem_Project4/Program.cs
@@ -7,3 +7,52 @@
 //   info = {2, 3, 3, 1 }
 //   выходные данные: 1, 7, 0, 1
 
+int[] data = new int[] { 0, 1, 1, 1, 1, 0, 0, 0, 1 };
+int[] info = new int[] { 2, 3, 3, 1 };
+
+PrintArray(ConvertDataToDecArray(data, info));
+// PrintArray(Function1(data, info));
+System.Console.WriteLine();
+
+int[] ConvertDataToDecArray(int[] dataArray, int[] infoArray)
+{
+    int count = 0;
+    int[] result = new int[infoArray.Length];
+    for (int i = 0; i < infoArray.Length; i++)
+    {
+        int[] bin = new int[infoArray[i]];
+        for (int j = 0; j < infoArray[i]; j++)
+        {
+            bin[j] = dataArray[count];
+            count++;
+        }
+        result[i] = BinToDec(bin);
+    }
+    return result;
+}
+
+
+int BinToDec(int[] array)
+{
+    Array.Reverse(array);
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        result = array[i] * Convert.ToInt32(Math.Pow(2, i)) + result;
+    }
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write(array[i]);
+        if (i == count - 1)
+            Console.WriteLine("]");
+        else
+            Console.Write(", ");
+    }
+}
