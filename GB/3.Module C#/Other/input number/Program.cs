a8e4bd34fcb8371c19a0a2effb0f610a118711a2
Oxpa <saweec9@gmail.com>
2022-10-18 03:41:55 +0300

    BD 1th lec

--- GB/3.Module C#/Other/input number/Program.cs
+++ GB/3.Module C#/Other/input number/Program.cs
@@ -8,3 +8,10 @@
             return number;
     }
 }
+
+        Console.WriteLine("Введите два числа через пробел: длина и ширина стола");
+        string line = Console.ReadLine();
+        string[] splitString = line.Split(' ');
+
+        double length = Convert.ToDouble(splitString[0]); // длина
+        double width = Convert.ToDouble(splitString[1]); // ширина
