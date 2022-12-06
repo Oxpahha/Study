91dd3e5efa539472067c04eb8ca4674b7e676f65
Oxpa <saweec9@gmail.com>
2022-12-06 21:50:17 +0300

    upd 06.12

--- GB/5.Module Python/4th seminar/Homework/pr1-5.py
+++ GB/5.Module Python/4th seminar/Homework/pr1-5.py
@@ -1,31 +1,28 @@
-# Решения оформляйте в виде функций
-# Используйте type hints и doctring
-
 # 1 - Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
 # N = 20 -> [2,5]
 # N = 30 -> [2, 3, 5]
 
-# n_list = []
-# n_num = int(input("N: "))
-# simple = 2
-# n_list.append(simple) 
-# while simple <= n_num:
-#     if n_num % simple == 0:
-#         if simple != 2:
-#             n_list.append(simple)
-#         n_num //= simple
-#         simple = 2
-#     else: simple += 1
-# print(n_list)
+n_list = []
+n_num = int(input("N: "))
+simple = 2
+n_list.append(simple) 
+while simple <= n_num:
+    if n_num % simple == 0:
+        if simple != 2:
+            n_list.append(simple)
+        n_num //= simple
+        simple = 2
+    else: simple += 1
+print(n_list)
 
 
 # 2 - Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности. Не использовать множества.
 # Постарайтесь решить за одно условие
 # [1,1,1,1,2,2,2,3,3,3,4] -> [1,2,3,4]
 
-# n_list = []
-# [n_list.append(i) for i in (list(map(int, input("Введите числа через запятую:\n").split(",")))) if i not in n_list]
-# print(n_list)
+n_list = []
+[n_list.append(i) for i in (list(map(int, input("Введите числа через запятую:\n").split(",")))) if i not in n_list]
+print(n_list)
 
 # 3 - В файле, содержащем фамилии студентов и их оценки, изменить на буквы в верхнем регистре тех студентов, которые имеют средний балл более «4».
 # Нужно перезаписать файл.
@@ -41,11 +38,9 @@
 # Фредди Меркури 3
 # Александр Пушкин 4
 
-# def read_file(file):
-#     data = open(file, 'r',encoding='UTF‑8')
-#     for line in data:
-#         print(line, end='')
-#     data.close()
+# Решения оформляйте в виде функций
+# Используйте type hints и doctring
+
 
 def return_line_str(file):
     data = open(file, 'r',encoding='UTF8')
