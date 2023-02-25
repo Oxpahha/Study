e824812877f0f7aea2f7e6eb0913e1182a8e6a1c
Oxpa <saweec9@gmail.com>
2023-02-25 06:34:35 +0300

    upd 25.02

---
+++ RedRover.school/Java/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        smile();
+
+    }
+    static void smile(){
+        /*
+        Вывести/напечатать смайлик (не :), а настоящий смайлик одним символом).
+         */
+
+        //U+1F642
+        //0xD83D 0xDE42
+        // \uD83D\uDE42\
+       String smile = "\uD83D\uDE42";
+                System.out.println(smile);
+    }
+}
