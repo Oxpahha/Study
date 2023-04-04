109485dadc9d041001c0c11833b9ce3f5b6c841a
Oxpa <saweec9@gmail.com>
2023-04-04 13:12:57 +0300

    upd 04.04

--- RedRover.school/Java/HWEleven/helpers/Generate.java
+++ RedRover.school/Java/HWEleven/helpers/Generate.java
@@ -6,27 +6,27 @@ import org.redrover.Worker;
 
 public class Generate {
     public static void generationEmpTestData(){
-        Manager one = new Manager();
-        one.setName("Jake","Broock");
-        one.setGender(1);
-        one.setBaseSalary(100);
-        one.setNumberOfSubordinates(12);
-        Storage.jake = one;
+        Manager jake = new Manager();
+        jake.setName("Jake","Broock");
+        jake.setGender(1);
+        jake.setBaseSalary(100);
+        jake.setNumberOfSubordinates(12);
+        Storage.one = jake;
 
-        Manager two = new Manager();
-        two.setName("John","Doe");
-        two.setGender(1);
-        two.setBaseSalary(100);
-        two.setNumberOfSubordinates(15);
-        Storage.john = two;
+        Manager john = new Manager();
+        john.setName("John","Doe");
+        john.setGender(1);
+        john.setBaseSalary(100);
+        john.setNumberOfSubordinates(15);
+        Storage.two = john;
 
-        Worker three = new Worker();
-        three.setName("Billy","Kent");
-        three.setGender(2);
-        three.setBaseSalary(50);
-        Storage.bill = three;
+        Worker bill = new Worker();
+        bill.setName("Billy","Kent");
+        bill.setGender(2);
+        bill.setBaseSalary(50);
+        Storage.three = bill;
 
-        Storage.empArray = new Employee[]{one, two, three};
-        Storage.managerArray = new Manager[]{one, two};
+        Storage.empArray = new Employee[]{jake, john, bill};
+        Storage.managerArray = new Manager[]{jake, john};
     }
 }
--- RedRover.school/Java/HWEleven/helpers/Storage.java
+++ RedRover.school/Java/HWEleven/helpers/Storage.java
@@ -2,15 +2,20 @@ package helpers;
 
 import org.redrover.Employee;
 import org.redrover.Manager;
-import org.redrover.Worker;
 
 public class Storage {
-
-    public static Manager jake;
-    public static Manager john;
-    public static Worker bill;
+    public static Object one;
+    public static Object two;
+    public static Object three;
     public static Employee[] empArray;
     public static Manager[] managerArray;
 
+    public static void clearEmpTest() {
+        Storage.one = null;
+        Storage.two = null;
+        Storage.three = null;
+        Storage.empArray = null;
+        Storage.managerArray = null;
+    }
 
 }
